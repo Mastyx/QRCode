28dacdcc440c1411d7379bd6949177c4fc70d4e7
mastyx <mastyx.house@gmail.com>
2024-08-03 11:43:50 +0200

    Ci siamo ... da aggiustare in seguiuto

--- createcode/js/main.js
+++ createcode/js/main.js
@@ -1,3 +1,8 @@
+const returnMainMenu = document.getElementById("main-menu");
+returnMainMenu.addEventListener("click", ()=>{
+	window.location.href = "../../index.html";
+})
+
 const generate = ()=> {
 	console.log('generate');
 	const text = document.getElementById("text").value;
