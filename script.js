6b83e5388e22e0036e508b20c5bdb23d21083d6b
mastyx <mastyx.house@gmail.com>
2024-08-10 09:53:40 +0200

    Risolto quasi tutto
    -inserito il testo delle info e collegato al pulsante appropriato
    -la div delle info viene creata e messa al centro dello scermo
    -la stessa non sovrasta il pulsante per farla scomparire

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 const btnScan = document.querySelector(".div-scann");
 const btnGenerate = document.querySelector(".div-generate");
 const info = document.getElementById("info");
-const divInfo = document.getElementById("dev-info")
+const divInfo = document.getElementById("div-info");
 
-btnScan.addEventListener("click", ()=>{
-	window.location.href = './scancode/scan.html';
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	caricaInfoTesto();	
+
+	btnScan.addEventListener("click", ()=>{
+		window.location.href = './scancode/scan.html';
+	});
+
+	btnGenerate.addEventListener("click", ()=>{
+		window.location.href = './createcode/generate.html';
+	});
+
+	info.addEventListener("click",()=>{
+		if (divInfo.style.display === 'none') {
+			divInfo.style.display = 'block';
+			info.style.background = "#FF0033"
+		} else {
+			divInfo.style.display = 'none';
+			info.style.background = '#333'
+		}
+	});
+
 });
 
-btnGenerate.addEventListener("click", ()=>{
-	window.location.href = './createcode/generate.html';
-});
 
-info.addEventListener("click",()=>{
-});
+// carica  il file di testo all'interno 
+// della div info
+
+const caricaInfoTesto = ()=>{
+	let textInfo = '';
+
+	fetch("info.txt")
+		.then(response => response.text())
+		.then(data => {
+			textInfo = data;
+			const areatesto = document.createElement("textarea");
+			areatesto.id = "area-testo-info";
+			areatesto.value = textInfo;
+			divInfo.appendChild(areatesto);
+
+		})
+		.catch(error => console.log("Errore caricamento info"));
+}
